26cfa3c60f6438dd315ac283a21a13c5faa3b24d
Julio Palma Quezada <j.palma.quezada@gmail.com>
2022-02-15 21:10:30 -0300

    Desafio Terminado

--- server.js
+++ server.js
@@ -3,29 +3,33 @@ const yargs = require('yargs');
 const Jimp = require('jimp');
 
 const app = express();
-
+const fs = require('fs');
 app.use(express.static('static'));
 
-app.get('/procesar', async (req,res) =>{
+app.get('/procesar', async(req, res) => {
     let archivo = req.query.foto;
     const imagen = await Jimp.read(archivo);
     await imagen.resize(350, Jimp.AUTO).rgba(false).quality(60).grayscale().writeAsync('img/newImg.jpg')
-    res.send('imagen procesada exitosamente');
+
+    fs.readFile('img/newImg.jpg', (err, imagen) => {
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' })
+        res.end(imagen)
+    })
+
 });
 
 let puerto = 8080;
 
 yargs.command(
     'start',
-    'comando para echar a correr el servidor',
-    {
+    'comando para echar a correr el servidor', {
         key: {
             describe: 'clave secreta para iniciar el servidor',
             demand: true,
             alias: 'k'
         }
     },
-    function (args) {
+    function(args) {
 
         if (args.key != '123') {
             console.log('clave incorrecta')
